e67ec3d72c4abaae39b281102684ab117d58abc7
Prish20 <contact@christadriansanya.me>
2024-12-12 17:21:47 +0300

    Update PPR in services page

--- app/(root)/services/page.tsx
+++ app/(root)/services/page.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 import BlurFade from "@/components/ui/blur-fade";
-import { client } from "@/sanity/lib/client";
+// import { client } from "@/sanity/lib/client";
 import { SERVICES_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
-const Page =async () => {
-  const projects = await client.fetch(SERVICES_QUERY);
+// eslint-disable-next-line camelcase
+export const experimental_ppr = true;
+
+const Page = async () => {
+  // const projects = await client.fetch(SERVICES_QUERY);
+  const { data: projects } = await sanityFetch({ query: SERVICES_QUERY });
   return (
     <div className="container">
       <BlurFade>
         <HoverEffect items={projects} />
       </BlurFade>
+      <SanityLive />
     </div>
   );
 };
